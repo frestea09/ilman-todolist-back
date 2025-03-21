98236ae0181b93627418922ccce74a09409657ba
ilman <ilmanfrasetya@gmail.com>
2025-03-21 07:33:13 +0700

    feat : add compression

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 import { VersioningType } from '@nestjs/common';
 import { ResponseInterceptor } from './response/response.interceptor';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,7 @@ async function bootstrap() {
     type: VersioningType.HEADER,
     header: 'Custom-Header',
   });
+  app.use(compression());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
